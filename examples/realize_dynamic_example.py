964cc7603e4f4918d5d8013916c485506c4ee4c5
islesarev317 <lolirei@mail.ru>
2023-07-06 00:56:59 +0300

    added examle

---
+++ examples/realize_dynamic_example.py
@@ -0,0 +1,44 @@
+import bpy
+import sys
+import os
+
+dir = os.path.dirname(bpy.data.filepath)
+if not dir in sys.path:
+    sys.path.append(dir)
+
+import tensor
+import utils
+import realize_dynamic
+
+import imp
+
+imp.reload(tensor)
+imp.reload(utils)
+imp.reload(realize_dynamic)
+
+from tensor import LocatedTensor
+from utils import print
+from realize_dynamic import InstanceDynamic
+
+# =============================================================
+
+a = LocatedTensor.zeros(corner=(1, 1, 1), dim=(5, 4, 3))
+a[1:4, 0:3] = 1
+a.hollow()
+#print(a)
+
+b = LocatedTensor.zeros(corner=(3, 3, 2), dim=(3, 3, 3))
+b[:] = 3
+#print(b)
+
+c = LocatedTensor.union(a, b)
+print(c)
+
+collection = bpy.data.collections["Collection"]
+default_image = bpy.data.objects["Cube"]
+grain = 0.5
+
+instance = InstanceDynamic(grain, collection, default_image)
+instance.update(a)
+instance.update(b)
+
