5b9a0f4b0bd9e91485770c7aa947fd9fbecf6010
islesarev317 <lolirei@mail.ru>
2023-12-05 13:42:18 +0300

    Suzanne.py (added comments)

--- examples/example-2312051257/Suzanne.py
+++ examples/example-2312051257/Suzanne.py
@@ -11,23 +11,27 @@ import imp
 import instance
 import virtual
 
-imp.reload(instance)
+imp.reload(instance)  # reload needs for usage in blender
 imp.reload(virtual)
 
 from instance import Instance
 from virtual import VirtualObject
 
-# =============================
+# ------------------------------------------------------------------------------------ #
+# Suzanne.
+# (In this example, I manually removed some noise around Suzanne,
+# which was due to tricky Suzanne's geometry.)
+# ------------------------------------------------------------------------------------ #
 
 # params
 frame_step = 1
 grain = 0.3
 limit_cells = 5000
-collection = bpy.data.collections["Cells"]
-default_image = bpy.data.objects["Image"]
+collection = bpy.data.collections["Cells"]  # collection for cells (need to be created before script starting)
+default_image = bpy.data.objects["Image"]  # object from which cells will be copied (need to be created beforehand)
 
 # objects
-suzanne = VirtualObject(bpy.data.objects["Suzanne"], grain).hollow()
+suzanne = VirtualObject(bpy.data.objects["Suzanne"], grain).hollow()  # using hollow for optimization
 
 # realize
 instance = Instance(suzanne, grain, collection, default_image, limit=limit_cells)
