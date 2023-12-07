a7257cba3f31261e80f14bc579fe1a52fb4cbacf
islesarev317 <lolirei@mail.ru>
2023-12-07 21:23:07 +0300

    hardcode optimization (skip zero rule) for tensor next_life

--- tensor.py
+++ tensor.py
@@ -267,8 +267,11 @@ class LocatedTensor:
         for global_point in tensor_rules.all_points_global:
             cell_rule = tensor_rules.get_global(global_point)
             cell_value = self.get_global(global_point, 0)
-            neighbors = self.num_alive(global_point)
-            next_cell_value = next_cell_func(self.ndim, cell_rule, cell_value, neighbors)
+            if cell_rule == 0:
+                next_cell_value = 0  # hardcode optimization! (skip zero rule)
+            else:
+                neighbors = self.num_alive(global_point)
+                next_cell_value = next_cell_func(self.ndim, cell_rule, cell_value, neighbors)
             tensor_next.set_global(global_point, next_cell_value)
 
         return tensor_next
