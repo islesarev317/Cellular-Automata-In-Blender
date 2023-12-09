867cbee4fb6f74d279be32ace6ceef7096ef8204
islesarev317 <lolirei@mail.ru>
2023-12-10 02:05:00 +0300

    updated demo Basic Cube

--- demos/demo-2312092116-Basic-Cube/Basic-Cube.py
+++ demos/demo-2312092116-Basic-Cube/Basic-Cube.py
@@ -26,8 +26,8 @@ from utils import catch_scene, clear_handlers, set_start_frame, show_label
 set_start_frame()  # set current frame to 1
 
 # params
-frame_step = 10                             # animation step
-grain = 0.4                                 # size of each cell
+frame_step = 15                             # animation step
+grain = 0.3                                 # size of each cell
 scale_factor = 0.9                          # to make gaps between cells
 limit = 5000                                # limit of cells for preventing overload
 bake = True                                 # make animation keys for optimization
@@ -37,8 +37,8 @@ info = bpy.data.collections["Info"]         # collection for info labels
 
 # rule
 code_max = CellRule.get_max_code()          # rule 18014398509481983 always keeps each cell alive
-code_rand = random.randrange(code_max)      # random rule
-code_maze = 17403076121982975               # rule which makes mazes
+code_rand = random.randrange(code_max//100) # random rule
+code_maze = 17403075121982975               # rule which makes mazes
 code_cond = CellRule.get_code(birth_cond=[4, 5, 6], survive_cond=[5, 6])  # from conditions
 code_range = CellRule.get_code(birth_cond=list(range(4, 9)), survive_cond=[16, 26])  # from ranges
 
@@ -50,6 +50,7 @@ cube = VirtualObject(bpy.data.objects["Cube"], grain)
 
 # expression
 vf_init = cube.random_fill([0, 1], weights=[0.8, 0.2])  # create cube with random values
+vf_init = vf_init.mirror()
 vf_rule = cube.fill(code_rand)                          # create cube with CA-rule values
 vf_life = VirtualLife(vf_rule, vf_init)                 # create CA-function
  
