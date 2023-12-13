0fbc8d1c57fca2b647aa92395a51aca06dcaba6e
islesarev317 <lolirei@mail.ru>
2023-12-13 16:10:14 +0300

    demo Basic Cube (refactoring)

--- demos/demo-2312092116-Basic-Cube/Basic-Cube.py
+++ demos/demo-2312092116-Basic-Cube/Basic-Cube.py
@@ -26,37 +26,37 @@ from utils import catch_scene, clear_handlers, set_start_frame, show_label
 set_start_frame()  # set current frame to 1
 
 # params
-frame_step = 15                             # animation step
-grain = 0.3                                 # size of each cell
-scale_factor = 0.9                          # to make gaps between cells
-limit = 5000                                # limit of cells for preventing overload
-bake = True                                 # make animation keys for optimization
+frame_step = 10   # animation step
+grain = 0.2  # size of each cell
+scale_factor = 0.9  # to make gaps between cells
+limit = 7000  # limit of cells for preventing overload
+bake = True  # make animation keys for optimization
 collection = bpy.data.collections["Cells"]  # collection to keep cells
-image = bpy.data.objects["Image"]           # object to copy cell from
-info = bpy.data.collections["Info"]         # collection for info labels
+image = bpy.data.objects["Image"]  # object to copy cell from
+info = bpy.data.collections["Info"]  # collection for info labels
 
 # rule
-code_max = CellRule.get_max_code()          # rule 18014398509481983 always keeps each cell alive
-code_flash = CellRule.get_flash_point()     # this and all codes greater result in uncontrolled growth
-code_rand = CellRule.randrange(0, 100)      # random rule by range, 100 - flash point, 200 max point
-code_maze = 17403075121982975               # rule which makes mazes
+code_max = CellRule.get_max_code()  # rule 18014398509481983 always keeps each cell alive
+code_flash = CellRule.get_flash_point()  # this and all codes greater result in uncontrolled growth
+code_rand = CellRule.randrange(0, 100)  # random rule by range, 100 - flash point, 200 max point
+code_maze = 8259390827203093  # some interesting rule
 code_cond = CellRule.get_code(birth_cond=[4, 5, 6], survive_cond=[5, 6])  # from conditions
 code_range = CellRule.get_code(birth_cond=list(range(4, 9)), survive_cond=[16, 26])  # from ranges
 
 # info
 msg_1 = "code_rand=" + str(code_rand)
 msg_2 = "percent=" + str(CellRule.get_percent(code_rand))
-show_label("1:", msg_1, collection=info) # number of random rule
-show_label("2:", msg_2, collection=info) # percentage of random code relative to flash point
+show_label("1:", msg_1, collection=info)  # number of random rule
+show_label("2:", msg_2, collection=info)  # percentage of random code relative to flash point
 
 # objects
 cube = VirtualObject(bpy.data.objects["Cube"], grain)
 
 # expression
-vf_init = cube.random_fill([0, 1], weights=[0.8, 0.2])  # create cube with random values
-vf_init = vf_init.mirror()                              # create symmetric matrix
-vf_rule = cube.fill(code_rand)                          # create cube with CA-rule values
-vf_life = VirtualLife(vf_rule, vf_init)                 # create CA-function
+vf_init = cube.random_fill([0, 1], weights=[0.9, 0.1])  # create cube with random values
+vf_init = vf_init.mirror()  # create symmetric matrix
+vf_rule = cube.fill(code_maze)  # you can change code_maze to code_rand!
+vf_life = VirtualLife(vf_rule, vf_init).hollow()  # create CA-function
  
 # realize
 instance = Instance(vf_life, grain, collection, image, bake, frame_step, limit)
